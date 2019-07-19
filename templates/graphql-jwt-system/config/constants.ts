eb93b22795b3c1e00965053182d311ac8381bb17
Anartz Mugika Ledo <mugan86@gmail.com>
2019-07-19 06:05:03 +0200

    Update

--- templates/graphql-jwt-system/config/constants.ts
+++
@@ -1,7 +0,0 @@
-import environments from './environments';
-
-if (process.env.NODE_ENV !== 'production') {
-    const environment = environments;
-}
-
-export const SECRET_KEY = process.env.SECRET || 'AnartzMugika';
